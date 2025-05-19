707528102191ac6ae809d57cac12f3d670414d49
hir01 <glazunovden@yandex.ru>
2025-05-19 15:36:49 +0300

    color

--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,34 @@ import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config';
 import Aura from '@primeuix/themes/aura';
+import {definePreset} from "@primeuix/themes";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 
+const MyPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '#FEE9CF',
+      100: '#FDE0BB',
+      200: '#FCCF94',
+      300: '#FABD6C',
+      400: '#F9AB45',
+      500: '#F8991D',
+      600: '#D67C07',
+      700: '#A05D05',
+      800: '#693D03',
+      900: '#331E02',
+      950: '#180E01'
+    }
+  }
+});
+
 app.use(PrimeVue, {
   theme: {
-    preset: Aura
+    preset: MyPreset
   }
 });
 
