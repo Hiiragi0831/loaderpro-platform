7ada39a6b28f29822ce26b4c01aec97622984553
hir01 <glazunovden@yandex.ru>
2025-05-26 15:06:09 +0300

    хеадер

--- components.d.ts
+++ components.d.ts
@@ -7,14 +7,22 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    Badge: typeof import('primevue/badge')['default']
     Button: typeof import('primevue/button')['default']
+    FloatLabel: typeof import('primevue/floatlabel')['default']
     HeaderApp: typeof import('./src/components/HeaderApp.vue')['default']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
+    Icon: typeof import('./src/components/Icon.vue')['default']
     IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
     IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
     IconEcosystem: typeof import('./src/components/icons/IconEcosystem.vue')['default']
+    IconImport: typeof import('./src/components/IconImport.vue')['default']
+    IconLogo: typeof import('./src/components/icons/IconLogo.vue')['default']
     IconSupport: typeof import('./src/components/icons/IconSupport.vue')['default']
     IconTooling: typeof import('./src/components/icons/IconTooling.vue')['default']
+    InputNumber: typeof import('primevue/inputnumber')['default']
+    InputText: typeof import('primevue/inputtext')['default']
+    OverlayBadge: typeof import('primevue/overlaybadge')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     TheWelcome: typeof import('./src/components/TheWelcome.vue')['default']
--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -1,13 +1,93 @@
 <script setup lang="ts">
+import IconLogo from '@/components/icons/IconLogo.vue'
 </script>
 
 <template>
   <header>
     <div class="py-18 bg-gray-100">
-      <div class="container m-auto grid grid-cols-2">
-        <div class="flex gap-60">
+      <div class="container m-auto flex justify-between">
+        <div class="flex gap-40">
           <RouterLink to="/">Обслуживание клиентов</RouterLink>
           <RouterLink to="/">Свяжитесь с нами</RouterLink>
+        </div>
+        <div class="flex gap-20 items-center">
+          <RouterLink to="/" class="flex">
+            <OverlayBadge value="2" size="small" class="size-20">
+              <i class="pi pi-bell" style="font-size: calc(20 * var(--width-multiplier))" />
+            </OverlayBadge>
+          </RouterLink>
+          <RouterLink to="/" class="flex gap-10">
+            <OverlayBadge value="2" size="small" class="size-20">
+              <i
+                class="pi pi-shopping-cart"
+                style="font-size: calc(20 * var(--width-multiplier))"
+              />
+            </OverlayBadge>
+            <div class="font-bold">0 ₽</div>
+          </RouterLink>
+        </div>
+      </div>
+    </div>
+    <div class="py-25 bg-white">
+      <div class="container m-auto flex justify-between">
+        <IconLogo class="w-200 h-44" />
+        <div class="flex gap-20 items-center">
+          <FloatLabel variant="on">
+            <InputText id="on_label" />
+            <label for="on_label">Бренд</label>
+          </FloatLabel>
+          <FloatLabel variant="on">
+            <InputText id="on_label" />
+            <label for="on_label">Номер запчасти</label>
+          </FloatLabel>
+          <FloatLabel variant="on">
+            <InputNumber  id="on_label" />
+            <label for="on_label">Количество</label>
+          </FloatLabel>
+          <Button label="Запросить цену" />
+        </div>
+        <div class="flex gap-10">
+          <div class="flex flex-col gap-10">
+            <div class="flex gap-10 items-center">
+              <i class="pi pi-user color-primary-500" />
+              <p>Имя пользователя</p>
+            </div>
+            <div class="text-sm">35465756200 - telkhpro 000</div>
+          </div>
+          <i class="pi pi-chevron-down" />
+        </div>
+      </div>
+    </div>
+    <div class="bg-gray-900">
+      <div class="container m-auto flex justify-between">
+        <div
+          class="flex"
+          v-for="(item, id) in [
+            [
+              { name: 'Подбор ЗЧ', url: '/' },
+              { name: 'Каталог', url: '/' },
+              { name: 'Гараж', url: '/' },
+            ],
+            [
+              { name: 'Запросы', url: '/' },
+              { name: 'Заказы', url: '/' },
+              { name: 'Новости', url: '/' },
+            ],
+          ]"
+          :key="id"
+        >
+          <RouterLink
+            class="group p-22 flex items-center justify-center transition hover:bg-primary-500 text-white text-xl"
+            v-for="(nav, navId) in item"
+            :key="navId"
+            :to="nav.url"
+          >
+            <span class="transition group-hover:opacity-0">{{ nav.name }}</span>
+            <span
+              class="transition font-bold opacity-0 absolute group-hover:scale-105 group-hover:opacity-100"
+              >{{ nav.name }}</span
+            >
+          </RouterLink>
         </div>
       </div>
     </div>
